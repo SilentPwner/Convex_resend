0ecdc4b5b0bac88f620c9f631b4f603fed0eaee6
SilentPwner <anmarkbrasly@gmail.com>
2025-08-03 17:44:00 +0300

    Delete lib directory

--- lib/auth.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from '@clerk/nextjs';
-
-export async function checkAdminRole() {
-  const { sessionClaims } = auth();
-  
-  // في تطبيق حقيقي، قد يكون هذا من قاعدة البيانات أو نظام المصادقة
-  return sessionClaims?.metadata.role === 'admin';
-}
